b5d82be5ba2ff4f5f47e90275001e851615aa5f4
Justin katasi <justin.katasi@capgemini.com>
2025-02-09 22:49:32 +0100

    relook home page

--- src/infrastructure/front-admin/src/app/home/home.component.ts
+++ src/infrastructure/front-admin/src/app/home/home.component.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common'
+
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     RouterOutlet,
     CommonModule,
-    RouterModule
+    RouterModule,
+    NgOptimizedImage
   ],
   templateUrl: './home.component.html',
 })
